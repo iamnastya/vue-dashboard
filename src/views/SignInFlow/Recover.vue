f0f798c181ffc2a2c75cdcd71d76f979fef3d3f7
Nastasia <nastya.nty@gmail.com>
2019-05-30 19:32:20 +0500

    added request and recovery pages and logic

--- src/views/SignInFlow/Recover.vue
+++ src/views/SignInFlow/Recover.vue
@@ -1,12 +1,96 @@
 <template>
-  <div></div>
+  <div
+    class="container"
+    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+  >
+    <RequestAccount/>
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode">
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode">
+      <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Recover account</h4>
+      <form @submit.prevent="onSubmit">
+        <input
+          type="email"
+          placeholder="Your Email"
+          :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+          v-model="email"
+          required
+        >
+        <button>Send email</button>
+      </form>
+      <router-link
+        to="/signin"
+        :class="{'light-link' : isDarkMode, 'dark-link' : !isDarkMode}"
+      >Already have an accoun? Sign In now</router-link>
+      <ThemeSwitch/>
+    </div>
+  </div>
 </template>
 
 <script>
+import RequestAccount from "@/components/RequestAccount";
+import ThemeSwitch from "@/components/ThemeSwitch";
+import { auth } from "@/main";
+
 export default {
-  name: "Recover"
+  name: "Recover",
+  components: {
+    RequestAccount,
+    ThemeSwitch
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      hasText: false,
+      text: ""
+    };
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  },
+  methods: {
+    onSubmit() {
+      const email = this.email;
+
+      auth
+        .requestPasswordRecovery(email)
+        .then(() => {
+          this.$router.push({
+            name: "signin",
+            params: {
+              userRecoveredAccount: true,
+              email: email
+            }
+          });
+        })
+        .catch(error => {
+          alert("Error: " + error);
+        });
+    }
+  },
+  mounted() {
+    const params = this.$route.params;
+
+    if (params.userLoggedOut) {
+      this.hasText = true;
+      this.text = "You have logged out!";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.login {
+  width: 400px;
+}
 </style>
--- src/views/SignInFlow/Request.vue
+++ src/views/SignInFlow/Request.vue
@@ -1,12 +1,105 @@
 <template>
-  <div></div>
+  <div
+    class="container"
+    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+  >
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode">
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode">
+      <h4
+        :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}"
+      >Enter your email to Sign up</h4>
+      <form @submit.prevent="onSubmit">
+        <input
+          type="email"
+          placeholder="Your Email"
+          :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+          v-model="email"
+          required
+        >
+        <button>Sign up</button>
+      </form>
+      <router-link
+        to="/signin"
+        :class="{'light-link' : isDarkMode, 'dark-link' : !isDarkMode}"
+      >Already have an accoun? Sign In now</router-link>
+      <ThemeSwitch/>
+    </div>
+  </div>
 </template>
 
 <script>
+import ThemeSwitch from "@/components/ThemeSwitch";
+
 export default {
-  name: "Request"
+  name: "Request",
+  components: {
+    ThemeSwitch
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      hasText: false,
+      text: ""
+    };
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  },
+  methods: {
+    onSubmit() {
+      const email = this.email;
+
+      // Slack API logic
+      let slackURL = new URL("https://slack.com/api/chat.postMessage");
+
+      const data = {
+        token:
+          "xoxp-652284770295-644103844785-650428799237-4bc1a04e32f8417bb58bc34616a3f113",
+        channel: "vue",
+        text: `${email} has requested admin access to Vue. Please go to Netlify to invite them.`
+      };
+
+      slackURL.search = new URLSearchParams(data);
+
+      fetch(slackURL)
+        .then(() => {
+          this.$router.push({
+            name: "signin",
+            params: {
+              userRequestedAccount: true,
+              email: email
+            }
+          });
+        })
+        .catch(error => {
+          alert("Error: " + error);
+        });
+    }
+  },
+  mounted() {
+    const params = this.$route.params;
+
+    if (params.userLoggedOut) {
+      this.hasText = true;
+      this.text = "You have logged out!";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.login {
+  width: 400px;
+}
 </style>
